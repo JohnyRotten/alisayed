fa1ecfb883e9f72a36fa6e30fe36fd98ef7cd216
JohnyRotten <mr.alegon@yandex.ru>
2015-11-05 16:38:43 +0300

    Sized

--- client/views/services.js
+++ client/views/services.js
@@ -1,15 +1,15 @@
 let services = [
   {
     title: 'Drawning Scketch',
-    body: new Lorem().generate(50)
+    body: new Lorem().generate(40)
   },
   {
     title: 'Making Design',
-    body: new Lorem().generate(50)
+    body: new Lorem().generate(40)
   },
   {
     title: 'Developing Product',
-    body: new Lorem().generate(50)
+    body: new Lorem().generate(40)
   }
 ];
 
